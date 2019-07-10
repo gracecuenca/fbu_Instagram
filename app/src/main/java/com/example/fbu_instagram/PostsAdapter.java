c374a760bba56e87619c01d22d941077e0fc8c1a
Grace Cuenca <gcuenca@princeton.edu>
2019-07-10 14:31:52 -0700

    added non functional buttons and timestamp

--- app/src/main/java/com/example/fbu_instagram/PostsAdapter.java
+++ app/src/main/java/com/example/fbu_instagram/PostsAdapter.java
@@ -2,6 +2,7 @@ package com.example.fbu_instagram;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,9 @@ import com.bumptech.glide.Glide;
 import com.example.fbu_instagram.model.Post;
 import com.parse.ParseFile;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
 
@@ -50,23 +53,42 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         private TextView tvHandle;
         private ImageView ivImage;
         private TextView tvDescription;
+        private TextView tvSmallHandle;
+        private TextView tvTimeStamp;
 
         public ViewHolder(View itemView){
             super(itemView);
             tvHandle = (TextView) itemView.findViewById(R.id.tvHandle);
             ivImage = (ImageView) itemView.findViewById(R.id.ivImage);
             tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
+            tvSmallHandle = (TextView) itemView.findViewById(R.id.tvSmallHandle);
+            tvTimeStamp = (TextView) itemView.findViewById(R.id.tvTimeStamp);
         }
 
         public void bind(Post post){
             tvHandle.setText(post.getUser().getUsername());
+            tvSmallHandle.setText(post.getUser().getUsername());
             ParseFile image = post.getImage();
             if(image != null){
                 Glide.with(context).load(image.getUrl()).into(ivImage);
             }
             tvDescription.setText(post.getDescription());
+            tvTimeStamp.setText(getRelativeTimeAgo(post));
         }
 
+    }
+
+    // calculating the relative time stamp
+    public String getRelativeTimeAgo(Post post) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        long dateMillis = post.getCreatedAt().getTime();
+        relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        return relativeDate;
     }
 
     // following methods needed for swipe to refresh
